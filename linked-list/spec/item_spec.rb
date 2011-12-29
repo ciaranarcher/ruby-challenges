dab852befbe2f929b13193609ee73af81101610d
Ciaran Archer <ciaran.archer@gmail.com>
2011-12-29 13:16:59 +0000

    spacings

--- linked-list/spec/item_spec.rb
+++ linked-list/spec/item_spec.rb
@@ -3,39 +3,39 @@ require 'log_buddy'
 
 
 module RubyChallenges
-	describe Item do
-		it 'should be created' do
-			Item.new
-		end
-		describe 'once created' do
-			before(:each) do
-			    @item = Item.new
-			end
-			it 'should respond to value message' do
-				@item.should respond_to :value
-			end
-			it 'should accept a string' do
-				test_val = 'hello!' 
-				@item.value = test_val
-				@item.value.should == test_val
-			end
-			it 'should accept a number' do
-				test_val = 1234 
-				@item.value = test_val
-				@item.value.should == test_val
-			end
-			it 'should respond to next_item message' do
-				@item.should respond_to :next_item
-			end
-			it 'should respond to prev_item message' do
-				@item.should respond_to :prev_item
-			end
-			it 'should repond to next_item with a reference to nil' do
-				@item.next_item.nil?.should == true
-			end
-			it 'should repond to prev_item with a reference to nil' do
-				@item.prev_item.nil?.should == true
-			end
-		end
-	end
+  describe Item do
+    it 'should be created' do
+      Item.new
+    end
+    describe 'once created' do
+      before(:each) do
+          @item = Item.new
+      end
+      it 'should respond to value message' do
+        @item.should respond_to :value
+      end
+      it 'should accept a string' do
+        test_val = 'hello!' 
+        @item.value = test_val
+        @item.value.should == test_val
+      end
+      it 'should accept a number' do
+        test_val = 1234 
+        @item.value = test_val
+        @item.value.should == test_val
+      end
+      it 'should respond to next_item message' do
+        @item.should respond_to :next_item
+      end
+      it 'should respond to prev_item message' do
+        @item.should respond_to :prev_item
+      end
+      it 'should repond to next_item with a reference to nil' do
+        @item.next_item.nil?.should == true
+      end
+      it 'should repond to prev_item with a reference to nil' do
+        @item.prev_item.nil?.should == true
+      end
+    end
+  end
 end
