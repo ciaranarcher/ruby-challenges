50beaf3184576af245d83d270217470100a09aac
Ciaran Archer <ciaran.archer@gmail.com>
2012-01-02 19:28:54 +0000

    simple iterative bubble sort working

--- bubble-sort/lib/bubble_sort.rb
+++ bubble-sort/lib/bubble_sort.rb
@@ -7,7 +7,43 @@ module RubyChallenges
     end
 
     def sort(arr)
-      []
+      begin
+        swap_done = compare_values(arr)
+      end while swap_done
+      arr
+    end
+
+    private
+
+    #def compare_values(arr, i=0)
+    #  d {arr.length; i; i + 1; @swap_done}
+    #
+    #  if (i + 1) < arr.length && i < arr.length
+    #    # try swap elements
+    #    if arr[i + 1] > arr[i]
+    #      swap_elements(arr[i], arr[i + 1])
+    #      @swap_done = true
+    #    end 
+    #    compare_values(arr, i + 1)
+    #  end
+    #end
+
+    def compare_values(arr)
+      swapped = false
+      
+      for i in (0..arr.length - 1)
+        begin
+          swap_elements(arr, i, i + 1) 
+          swapped = true
+        end if (!arr[i + 1].nil? && arr[i] > arr[i + 1])
+      end
+      swapped
+    end
+
+    def swap_elements(arr, i, j)
+      temp = arr[i]
+      arr[i] = arr[j]
+      arr[j] = temp
     end
   end
 end
--- bubble-sort/spec/bubble_sort_spec.rb
+++ bubble-sort/spec/bubble_sort_spec.rb
@@ -16,7 +16,26 @@ module RubyChallenges
       it 'should return an empty array' do
         @sorter.sort(@target).should == []
       end
-      
+    end
+    describe 'when there is an array of one element' do
+      before(:each) do
+        @sorter = BubbleSort.new
+        @target = [1]
+      end
+      it 'should return the same array' do
+        @sorter.sort(@target).should == [1]
+      end
+    end
+    describe 'when there is an array of more than one element' do
+      before(:each) do
+        @sorter = BubbleSort.new
+        @target1 = [1, 3, 2]
+        @target2 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+      end
+      it 'should return a sorted array' do
+        @sorter.sort(@target1).should == [1, 2, 3]
+        @sorter.sort(@target2).should == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      end
     end
   end
 end
