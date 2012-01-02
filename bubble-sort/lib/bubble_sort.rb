aefba68b2e4893a55d229d0d1ee1ad3d209a388e
Ciaran Archer <ciaran.archer@gmail.com>
2012-01-02 19:36:27 +0000

    Revert "Revert "simple iterative bubble sort working""
    This reverts commit 8540f46fbc171ae86a8d5fa032087bbe87d3664d.

--- bubble-sort/lib/bubble_sort.rb
+++ bubble-sort/lib/bubble_sort.rb
@@ -6,29 +6,36 @@ module RubyChallenges
       self
     end
 
-    def sort(arr)
-      begin
-        swap_done = compare_values(arr)
-      end while swap_done
+    def sort(arr, mode='recursive')
+      if mode == 'recursive'
+        begin
+          swap_done = compare_values_recursive(arr)
+        end while swap_done
+      else
+        begin
+          swap_done = compare_values_recursive(arr)
+        end while swap_done 
+      end
+      
       arr
     end
 
     private
 
-    #def compare_values(arr, i=0)
-    #  d {arr.length; i; i + 1; @swap_done}
-    #
-    #  if (i + 1) < arr.length && i < arr.length
-    #    # try swap elements
-    #    if arr[i + 1] > arr[i]
-    #      swap_elements(arr[i], arr[i + 1])
-    #      @swap_done = true
-    #    end 
-    #    compare_values(arr, i + 1)
-    #  end
-    #end
+    def compare_values_recursive(arr, i=0, swap_done=false)
+    
+      if (i + 1) < arr.length && i < arr.length
+        # try swap elements
+        if arr[i + 1] > arr[i]
+          swapped = swap_elements(arr[i], arr[i + 1])
+          swap_done = true if swapped
+        end 
+        compare_values_recursive(arr, i + 1, swap_done)
+      end
+      swap_done
+    end
 
-    def compare_values(arr)
+    def compare_values_iterative(arr)
       swapped = false
       
       for i in (0..arr.length - 1)
