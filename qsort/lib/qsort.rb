7e24d481123944caf32562c28104899d83563dce
Ciaran Archer <ciaran.archer@gmail.com>
2012-01-04 21:07:05 +0000

    started qsort, need to do partition operation next

---
+++ qsort/lib/qsort.rb
@@ -0,0 +1,19 @@
+require 'log_buddy'
+
+module RubyChallenges
+  class QSort
+    attr_accessor :arr
+
+    def initialize
+      self
+    end
+    
+    def sort
+      return @arr if (0..1).include?(@arr.length)
+    end
+
+    def partition 
+      
+    end
+  end
+end
---
+++ qsort/spec/qsort_spec.rb
@@ -0,0 +1,44 @@
+require_relative '../lib/qsort'
+require 'log_buddy'
+
+
+module RubyChallenges
+  describe QSort do
+    it 'should be created OK' do
+      QSort.new
+    end
+    describe 'when there is an empty array' do
+      before (:each) do
+        @sorter = QSort.new
+        @target = []
+      end
+      it 'should return an empty array' do
+        @sorter.arr = @target
+        @sorter.sort.should == []
+      end
+    end
+    describe 'when there is an array of one element' do
+      before (:each) do
+        @sorter = QSort.new
+        @target = [1]
+      end
+      it 'should return an empty array' do
+        @sorter.arr = @target
+        @sorter.sort.should == [1]
+      end
+    end
+    describe 'when there is an array of three elements [1, 3, 2]' do
+      before (:each) do
+        @sorter = QSort.new
+        @target = [1, 3, 2]
+      end
+      it 'should return a pivot value' do 
+        
+      end
+      it 'should return a sorted array' do
+        @sorter.arr = @target
+        @sorter.sort().should == [1, 2, 3]
+      end
+    end
+  end
+end
